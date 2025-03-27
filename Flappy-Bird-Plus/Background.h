71d5e28df1d4a831bd7a7abfd739679a238fbe11
Phan Văn Phước Hưng <phanhungutral@gmail.com>
2025-03-27 09:58:52 +0700

    Create window and button
    Add soundfx and images

---
+++ Flappy-Bird-Plus/Background.h
@@ -0,0 +1,53 @@
+#ifndef BACKGROUND_H_INCLUDED
+#define BACKGROUND_H_INCLUDED
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <cstdio>
+#include <cstdlib>
+#include "RenderWindow.h"
+#include "Entity.h"
+#include "Bird.h"
+#include "Pipe.h"
+#include "Button.h"
+
+class Background:public Entity
+{
+	int mode;
+	SDL_Rect currentFrame;
+public:
+	 Background(SDL_Texture* p_tex,int mode):Entity(0,0,p_tex,2){
+	 	printf("Background created\n");
+	 	if(mode){
+	 	currentFrame.x=0;
+		currentFrame.y=50;
+		currentFrame.w=140;
+		currentFrame.h=166;
+	 	}
+	 	else{
+	 	currentFrame.x=300;
+		currentFrame.y=0;
+		currentFrame.w=60;
+		currentFrame.h=55;
+		// increaseZoom();
+	 	}
+	 	if(mode==2) {currentFrame.y=0;
+	 	currentFrame.h=216;
+	 	}
+		setcurrFrame(currentFrame);
+	 }
+	void change_mode(){
+		currentFrame=getFrame();
+		currentFrame.x=((currentFrame.x==0)?(147+currentFrame.x):(currentFrame.x-147));
+		setcurrFrame(currentFrame);
+	}
+};
+
+void draw_background(Background &background1,Background &background2,RenderWindow &window,int pos);
+int gameplay(RenderWindow &window,SDL_Texture* bg,Mix_Chunk* flapping_sound,Mix_Chunk* die_sound,Mix_Chunk* hit_sound,Mix_Chunk* point_sound,int &highscore);
+int retry(RenderWindow &window,int score,SDL_Texture* bg,int &highscore);
+int main_menu(RenderWindow &window,SDL_Texture* bg);
+
+
+#endif // BACKGROUND_H_INCLUDED
--- Flappy-Bird-Plus/main.cpp
+++ Flappy-Bird-Plus/main.cpp
@@ -1,38 +1,22 @@
-//Using SDL, SDL_image, standard IO, and strings
 #include <SDL.h>
 #include <SDL_image.h>
 #include <stdio.h>
 #include <string>
-#include "constants.h"
-#include "LTexture.h"
-#include "Dot.h"
-#include "LTimer.h"
 
-//The dot that will move around on the screen
-
-//Starts up SDL and creates window
 bool init();
 
-//Loads media
 bool loadMedia();
 
-//Frees media and shuts down SDL
 void close();
 
-//The window we'll be rendering to
 SDL_Window* gWindow = NULL;
 
-//The window renderer
 SDL_Renderer* gRenderer = NULL;
-
-//Scene textures
 
 bool init()
 {
-	//Initialization flag
 	bool success = true;
 
-	//Initialize SDL
 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
 	{
 		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
@@ -40,14 +24,12 @@ bool init()
 	}
 	else
 	{
-		//Set texture filtering to linear
 		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
 		{
 			printf( "Warning: Linear texture filtering not enabled!" );
 		}
 
-		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		gWindow = SDL_CreateWindow( "Flappy bird plus", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 		if( gWindow == NULL )
 		{
 			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
@@ -55,7 +37,6 @@ bool init()
 		}
 		else
 		{
-			//Create vsynced renderer for window
 			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
 			if( gRenderer == NULL )
 			{
@@ -64,10 +45,8 @@ bool init()
 			}
 			else
 			{
-				//Initialize renderer color
 				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 
-				//Initialize PNG loading
 				int imgFlags = IMG_INIT_PNG;
 				if( !( IMG_Init( imgFlags ) & imgFlags ) )
 				{
@@ -83,23 +62,11 @@ bool init()
 
 bool loadMedia()
 {
-	//Loading success flag
-	bool success = true;
 
-	//Load dot texture
-	if( !gDotTexture.loadFromFile( "dot.bmp" ) )
-	{
-		printf( "Failed to load dot texture!\n" );
-		success = false;
-	}
-
-	return success;
 }
 
 void close()
 {
-	//Free loaded images
-	gDotTexture.free();
 
 	//Destroy window
 	SDL_DestroyRenderer( gRenderer );
@@ -107,105 +74,49 @@ void close()
 	gWindow = NULL;
 	gRenderer = NULL;
 
-	//Quit SDL subsystems
 	IMG_Quit();
 	SDL_Quit();
 }
 
 int main( int argc, char* args[] )
 {
-	//Start up SDL and create window
 	if( !init() )
 	{
 		printf( "Failed to initialize!\n" );
 	}
 	else
 	{
-		//Load media
 		if( !loadMedia() )
 		{
 			printf( "Failed to load media!\n" );
 		}
 		else
 		{
-			//Main loop flag
 			bool quit = false;
 
-			//Event handler
 			SDL_Event e;
 
-			//The dot that will be moving around on the screen
-			Dot dot;
-
-			//While application is running
 			while( !quit )
 			{
-				//Handle events on queue
 				while( SDL_PollEvent( &e ) != 0 )
 				{
-					//User requests quit
 					if( e.type == SDL_QUIT )
 					{
 						quit = true;
 					}
 
-					//Handle input for the dot
-					dot.handleEvent( e );
 				}
-
-				//Move the dot
-				dot.move();
 
 				//Clear screen
 				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 				SDL_RenderClear( gRenderer );
 
-				//Render objects
-				dot.render();
-
-				//Update screen
 				SDL_RenderPresent( gRenderer );
 			}
 		}
 	}
 
-	//Free resources and close SDL
 	close();
 
 	return 0;
 }
-
-
-//#include <SDL.h>
-//#include <SDL_image.h>
-//#include "Game.h"
-//
-//Game* game = nullptr;
-//
-//int main(int argc, char* argv[]) {
-//    const int FPS = 60;
-//    const int frameDelay = 1000 / FPS;
-//
-//    Uint32 frameStart;
-//    int frametime;
-//
-//    game = new Game();
-//
-//    game->init("Super ninja", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-//               800, 640, false);
-//
-//    while (game->running()) {
-//        frameStart = SDL_GetTicks();
-//
-//        game->handleEvents();
-//        game->update();
-//        game->render();
-//
-//        frametime = SDL_GetTicks() - frameStart;
-//        if (frameDelay > frametime) {
-//            SDL_Delay(frameDelay - frametime);
-//        }
-//    }
-//    game->clean();
-//    return 0;
-//}
